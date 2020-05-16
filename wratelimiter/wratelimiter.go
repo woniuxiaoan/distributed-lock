d5f1c0468fdd8e4656d2e63840139a4280f25a34
蜗牛 <woniuxiaoan@MacBook-Pro.local>
2020-05-16 16:26:35 +0800

    update

--- wratelimiter/wratelimiter.go
+++ wratelimiter/wratelimiter.go
@@ -2,34 +2,49 @@ package wratelimiter
 
 import (
 	"errors"
+	"fmt"
 	"gopkg.in/redis.v5"
+	"math/rand"
+	"strconv"
 	"time"
 )
 
 const Script = `
+local function trim(origin, tag)
+   local result = origin
+   if string.len(tag) ~= 0 then
+       local begin, _ = string.find(origin, tag)
+       if begin ~= nil then
+         result = string.sub(origin, 1, begin - 1)
+       end
+   end
+   return result
+end
+
+local hash_tag = ARGV[6]
 local token_num_key = KEYS[1]
 local token_num_last_updated_time_key = KEYS[2]
-local token_required_time = tonumber(ARGV[1])
-local token_num_capacity = tonumber(ARGV[2])
-local duration = tonumber(ARGV[3])
-local token_num_required = tonumber(ARGV[4])
-local token_generated_rate = tonumber(ARGV[5])
+local token_required_time = tonumber(trim(ARGV[1],hash_tag))
+local token_num_capacity = tonumber(trim(ARGV[2],hash_tag))
+local ttl = tonumber(trim(ARGV[3],hash_tag))
+local token_num_required = tonumber(trim(ARGV[4],hash_tag))
+local token_generated_rate = tonumber(trim(ARGV[5],hash_tag))
 local token_num_last_updated_time = tonumber(redis.call("get", token_num_last_updated_time_key))
 
 if token_num_last_updated_time == nil then
   token_num_last_updated_time = token_required_time
 end
 
-local new_token = math.min(token_num_capacity, (token_required_time-token_num_last_updated_time) * token_generated_rate)
+local new_token = math.min(token_num_capacity, (token_required_time-token_num_last_updated_time) / token_generated_rate)
 
 local token_num = tonumber(redis.call("get", token_num_key))
 if token_num == nil then
-  token_num = 0
+  token_num = token_num_capacity
 end
 
 token_num = math.min(token_num_capacity, token_num+new_token)
 
-local allowed = token_num_required <= token_num
+local allowed = token_num >= 1
 
 if allowed then
   token_num = token_num - token_num_required
@@ -37,16 +52,14 @@ end
 
 token_num_last_updated_time = token_required_time
 
-local ttl = math.floor(2*duration/1000000000)
-
 redis.call("setex", token_num_key, ttl, token_num)
 redis.call("setex", token_num_last_updated_time_key, ttl, token_num_last_updated_time)
 
 if allowed then
-  return token_num
+  return {token_num, 1}
 end
 
-return -1
+return {token_num, 0}
 `
 
 var hashScript string
@@ -54,19 +67,23 @@ var hashScript string
 type Limiter struct {
 	uniqueKey      string
 	max            int64
-	duration       time.Duration
 	redisClient    *redis.Client
-	rate           float32
-	reloadDuration time.Duration
+	rate           int64
+	reloadDurationSeconds int
+	refreshDurationSeconds int
+	hashTag string
+	rand *rand.Rand
 }
 
 type LimiterSetting struct {
 	UniqueKey            string
 	Max                  int64
-	Duration             time.Duration
-	ScriptReloadDuration time.Duration
+	DurationMicrosecondsPerToken     int64
+	ScriptReloadSeconds int
+	RefreshDurationSeconds int
 	RedisAddr            string
 	RedisPwd             string
+	RedisClusterMod bool
 }
 
 func NewLimiter(setting *LimiterSetting) *Limiter {
@@ -76,30 +93,53 @@ func NewLimiter(setting *LimiterSetting) *Limiter {
 	})
 
 	go func() {
-		ticker := time.Tick(setting.ScriptReloadDuration)
+		ticker := time.Tick(time.Second * time.Duration(setting.ScriptReloadSeconds))
 		for {
 			<-ticker
 			redisClient.ScriptLoad(Script)
 		}
 	}()
 
-	return &Limiter{
+	limiter := &Limiter{
 		uniqueKey:   setting.UniqueKey,
 		max:         setting.Max,
-		duration:    setting.Duration,
 		redisClient: redisClient,
-		rate:        (float32(setting.Max) * float32(time.Millisecond)) / float32(setting.Duration),
+		rate:        setting.DurationMicrosecondsPerToken,
+		refreshDurationSeconds: setting.RefreshDurationSeconds,
+		rand: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
+
+	if setting.RedisClusterMod {
+		limiter.hashTag = fmt.Sprintf("|{%s}", limiter.generateFingerPrint())
+	}
+
+	return limiter
 }
 
-func (l *Limiter) GenerateKeys() []string {
-	return []string{
-		l.uniqueKey + "|" + "token_num",
+func (l *Limiter) generateFingerPrint() string {
+	return strconv.FormatUint(l.rand.Uint64(), 10)
+}
+
+func (l *Limiter) generateKeys() []string {
+	keys := []string{
+		l.uniqueKey + "|" + "token_num" ,
 		l.uniqueKey + "|" + "token_num_last_updated_key",
 	}
+
+	if len(l.hashTag) != 0 {
+		for i := 0; i<len(keys); i++ {
+			keys[i] = fmt.Sprintf("%v%s", keys[i], l.hashTag)
+		}
+	}
+
+	return keys
 }
 
-func (l *Limiter) RequestTokens(tokeNums int64) (int64, error) {
+func (l *Limiter) RequestTokens(tokeNumsRequired int64) (int64, error) {
+	if tokeNumsRequired > l.max {
+		return -1, fmt.Errorf("允许请求最大值为%d", l.max)
+	}
+
 	if len(hashScript) == 0 {
 		res, err := l.redisClient.ScriptLoad(Script).Result()
 		if err != nil {
@@ -108,15 +148,40 @@ func (l *Limiter) RequestTokens(tokeNums int64) (int64, error) {
 		hashScript = res
 	}
 
-	requiredTime := time.Now().UnixNano() / int64(time.Millisecond)
-	res, err := l.redisClient.EvalSha(hashScript, l.GenerateKeys(), requiredTime, l.max, int64(l.duration), tokeNums, l.rate).Result()
+	requiredTime := time.Now().UnixNano() / int64(time.Microsecond)
+	interfaces, err := l.redisClient.EvalSha(hashScript, l.generateKeys(), l.generateArgs(requiredTime, tokeNumsRequired)...).Result()
 	if err != nil {
 		return -1, err
 	}
 
-	if res.(int64) == -1 {
-		return -1, errors.New("令牌不够")
+	res, ok := interfaces.([]interface{})
+	if !ok {
+		return -1, errors.New("transfer interfaces failed")
 	}
 
-	return res.(int64), nil
+	tokensLeft, pass := res[0].(int64), res[1].(int64) == 1
+	if !pass {
+		return tokensLeft, errors.New("令牌不足")
+	}
+
+	return tokensLeft, nil
+}
+
+func (l *Limiter) generateArgs(requiredTime int64, tokenNumsRequired int64) []interface{} {
+	args := []interface{}{
+		requiredTime,
+		l.max,
+		l.refreshDurationSeconds,
+		tokenNumsRequired,
+		l.rate,
+		l.hashTag,
+	}
+
+	if len(l.hashTag) != 0 {
+		for i := 0; i < len(args) - 1; i++ {
+			args[i] = fmt.Sprintf("%v%s", args[i], l.hashTag)
+		}
+	}
+
+	return args
 }
--- wratelimiter/wratelimiter_test.go
+++ wratelimiter/wratelimiter_test.go
@@ -2,22 +2,23 @@ package wratelimiter
 
 import (
 	"testing"
-	"time"
 )
 
 func TestLimiterRequest(t *testing.T) {
 	uniqueKey := "qrain|get|files"
 	setting := &LimiterSetting{
 		UniqueKey:           uniqueKey,
-		Max:                  5,
-		Duration:             time.Second * 10,
-		ScriptReloadDuration: time.Second * 5,
+		Max:                  10,
 		RedisAddr:            "localhost:6379",
 		RedisPwd:             "",
+		DurationMicrosecondsPerToken: 1000000,
+		RefreshDurationSeconds: 60,
+		ScriptReloadSeconds: 10,
+		RedisClusterMod: true,
 	}
 	limiter := NewLimiter(setting)
 
-	if tokens, err := limiter.RequestTokens(1); err != nil {
+	if tokens, err := limiter.RequestTokens(6); err != nil {
 		t.Logf("request token failed: %v\n", err)
 	} else {
 		t.Logf("succeed,剩余%v个令牌", tokens)
